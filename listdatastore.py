1b08a1b9e7b3771e12223a772aec8fbb7a9cde12
Sunita <sunitav@gmail.com>
2022-06-21 16:18:13 -0700

    Create listdatastore.py

---
+++ listdatastore.py
@@ -0,0 +1,34 @@
+import os
+import time
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--data_dir', type=str)
+args = parser.parse_args()
+dir_list = os.listdir(args.data_dir)
+time.sleep(60)
+print(dir_list)
+
+print("all the contents of datastore")
+contents = list(Path(args.data_dir).iterdir())
+print(contents)
+
+print("all the contents of datastore usingos.walk")
+for path,dirs,files in os.walk(args.data_dir):
+    for filename in files:
+        print( os.path.join(path,filename))
+
+
+#print_directory_contents(args.data_dir)
+
+#def print_directory_contents(args.data_dir):
+#  for sChild in os.listdir(args.data_dir):
+#    sChildPath = os.path.join(args.data_dir,sChild)
+#    if os.path.isdir(sChildPath):
+#        print_directory_contents(sChildPath)
+#    else:
+#        print(sChildPath)
+#print("all the contents of datastore")
+#print_directory_contents(args.data_dir)
+~
